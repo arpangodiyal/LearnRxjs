7365751d4fdc88f15212a7ebb58ed18eb581e5c6
Arpan Godiyal <arpangodiyal24@gmail.com>
2019-02-15 20:23:29 +0530

    Commit

--- app.ts
+++ app.ts
@@ -3,8 +3,24 @@ import * as Rx from 'rxjs';
 
 const el = document.querySelector('.btn');
 
-Rx.fromEvent(el,'click')
-    .subscribe((
-        next=>console.log(next)
-    ))
+const obs1 = {
+    next: (s:any)=>{
+        console.log('I am observer 1');
+    }
+}
+
+const obs2 = {
+    next: (s:any)=>{
+        console.log('I am observer 2');
+    }
+}
+
+var observable = Rx.fromEvent(el,'click');
+let subscriber1 = observable.subscribe(obs1);
+let subscriber2 = observable.subscribe(obs2);
+
+setTimeout(()=>{
+    subscriber2.unsubscribe();
+}, 2000);
+
 
